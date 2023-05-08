36180074376f54b2bb85538781f0888a3b22f333
Miguel Blanco <78479657+miguebeto@users.noreply.github.com>
2023-05-08 16:09:49 -0500

    add styles responsive in header

---
+++ pages/corp/index.tsx
@@ -0,0 +1,11 @@
+import Layout from "../../components/general/layout/layout.component";
+
+const index = () => {
+  return (
+    <Layout>
+      <div>index</div>
+    </Layout>
+  );
+};
+
+export default index;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+/**
+ * @type {import('@types/tailwindcss/tailwind-config').TailwindConfig}
+ */
 module.exports = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -11,6 +15,6 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
 
