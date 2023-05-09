7ad2a70496d070a48c1fec509beff72e7e2c9ce9
andres2082113 <andres@kru360.com>
2023-05-09 01:15:00 -0500

    feat: added background in kitchen.component

--- components/views/home/kitchen/kitchen.component.tsx
+++ components/views/home/kitchen/kitchen.component.tsx
@@ -3,13 +3,11 @@ import styles from "./kitchen.module.css";
 
 const Kitchen = () => {
   return (
-    <div
-      className={
-        "flex flex-col content-center items-center py-48 w-full " +
-        styles.container_text
-      }
+
+    <div className={styles.container}>
+    <div className={"flex flex-col content-center items-center py-48 w-full " + styles.container_text}
     >
-      <h2 className={"text-8xl font-black text-white leading-3" + styles.title_main}>
+      <h2 className={"text-8xl font-black text-white leading-3 pb-10 " + styles.title_main}>
         PERSONALIZAMOS
       </h2>
       <h3 className={"text-8xl pl-4 font-medium text-white " + styles.title_main}>
@@ -17,6 +15,7 @@ const Kitchen = () => {
       </h3>
 
       <Slider/>
+    </div>
     </div>
 
 
