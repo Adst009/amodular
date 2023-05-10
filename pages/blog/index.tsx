c7890c754f7d0f626c970003c53b153dabb1a064
andres2082113 <andres@kru360.com>
2023-05-10 16:08:46 -0500

    feat:adjust

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Layout from "../../components/general/layout/layout.component";
+import Blog from "../../components/views/blog/blog.component";
 
 
 
@@ -9,7 +10,8 @@ const Index: NextPage = () => {
     return (
         <>
         <Layout>
-                
+            
+           <Blog />     
             
         </Layout>
       
