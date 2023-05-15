559f61f29b4793af8b7a8f1c5a9c6d3c4ed295fa
Miguel Blanco <78479657+miguebeto@users.noreply.github.com>
2023-05-15 11:02:24 -0500

    changes

--- components/views/home/realidad/realidad.tsx
+++ components/views/home/realidad/realidad.tsx
@@ -4,10 +4,10 @@ export default function Realidad() {
     return (
         <>
             <section className={styles.section}>
-                <div className={styles.general}>
-                    <div className={styles.content_1}>
-                        <h2>HACEMOS </h2>
-                        <h3>tu idea realidad</h3>
+                <div className={"flex flex-col md:flex-col lg:flex-row " + styles.general}>
+                    <div className={"md:pt-24 " + styles.content_1}>
+                        <h2 className="text-[48px] md:text-7xl leading-10">HACEMOS </h2>
+                        <h3 className="text-[48px] md:text-6xl leading-10">tu idea realidad</h3>
                         <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
                             tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis
                             nostrud exerci tation |ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
@@ -15,7 +15,7 @@ export default function Realidad() {
                     </div>
 
                     <div className={styles.content_2}>
-                        <h2 className="pr-20 leading-[60px]">TU COCINA SERÁ COMO IMAGINAS</h2>
+                        <h2 className="text-[39px] md:text-6xl md:pr-20 md:leading-[60px]">TU COCINA SERÁ COMO IMAGINAS</h2>
 
                     <a href="#">CONTÁCTANOS</a>
                     </div>
