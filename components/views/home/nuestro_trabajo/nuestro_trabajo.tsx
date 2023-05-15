adb142f469a2f97c8458762d0d6c8e53f3ce08e1
Miguel Blanco <78479657+miguebeto@users.noreply.github.com>
2023-05-15 15:44:30 -0500

    feat: add link epic 1

--- components/views/home/nuestro_trabajo/nuestro_trabajo.tsx
+++ components/views/home/nuestro_trabajo/nuestro_trabajo.tsx
@@ -1,80 +1,154 @@
-import styles from './styles.module.css'; 
-import {SwiperSlide} from "swiper/react";
-import SwiperJs from './swiperJs/swiperJs'; 
+import styles from "./styles.module.css";
+import { SwiperSlide } from "swiper/react";
+import SwiperJs from "./swiperJs/swiperJs";
 
 export default function NuestroTrabajo() {
-    return (
-        <>
-            <section className={styles.section}>
-                <div className={"relative w-10/12 flex-col lg:flex lg:flex-row items-center justify-center gap-14 " + styles.general}>
-                    <div className={styles.carrucel}>
-                        <SwiperJs>
-                            <SwiperSlide>
-                                <div className={styles.card}>
-                                    <img src="/images/Personalize/res1.png" alt=""/>
-                                    <div className={styles.callToAction}>
-                                        <i className='bx bx-plus'></i>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-
-                            <SwiperSlide>
-                                <div className={styles.card}>
-                                    <img src="/images/Personalize/res1.png" alt=""/>
-                                    <div className={styles.callToAction}>
-                                        <i className='bx bx-plus'></i>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className={styles.card}>
-                                    <img src="/images/Personalize/res1.png" alt=""/>
-                                    <div className={styles.callToAction}>
-                                        <i className='bx bx-plus'></i>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className={styles.card}>
-                                    <img src="/images/Personalize/res1.png" alt=""/>
-                                    <div className={styles.callToAction}>
-                                        <i className='bx bx-plus'></i>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className={styles.card}>
-                                    <img src="/images/Personalize/res1.png" alt=""/>
-                                    <div className={styles.callToAction}>
-                                        <i className='bx bx-plus'></i>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                            <SwiperSlide>
-                                <div className={styles.card}>
-                                    <img src="/images/Personalize/res1.png" alt=""/>
-                                    <div className={styles.callToAction}>
-                                        <i className='bx bx-plus'></i>
-                                    </div>
-                                </div>
-                            </SwiperSlide>
-                        </SwiperJs>
-                    </div>
-                    <div className={styles.content}>
-                        <h2><b>Nuestro</b> <br/> <span>trabajo</span></h2>
-                        <p>Somos una empresa dedicada al diseño y construcción de espacios,
-                            encaminados a lograr un lugar cómodo de acuerdo a tus necesidades.
-                            Nuestro principal objetivo es recrear cada diseños de manera autónoma,
-                            teniendo en cuenta la inversión y así,  poder ejecutar los resultados de cada proyecto de la mejor manera.
-                        </p>
-                        <a href="#">Contáctanos</a>
-                       <div className={styles.mundo}>
-                           <img src="/images/apoyo_grafico/mundo.png" alt=""/>
-                       </div>
-                    </div>
-
+  return (
+    <>
+      <section className={styles.section}>
+        <div
+          className={
+            "relative w-10/12 flex-col lg:flex lg:flex-row items-center justify-center gap-14 " +
+            styles.general
+          }
+        >
+          <div className={styles.carrucel}>
+            <SwiperJs>
+              <SwiperSlide>
+                <div className={styles.card}>
+                  <img src="/images/Personalize/res1.png" alt="" />
+                  <div className={styles.callToAction}>
+                    <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="fill-white"
+                    >
+                      <title />
+                      <g data-name="Layer 2" id="Layer_2">
+                        <path d="M28,28.5H4a.5.5,0,0,1-.5-.5V4A.5.5,0,0,1,4,3.5H28a.5.5,0,0,1,.5.5V28A.5.5,0,0,1,28,28.5Zm-23.5-1h23V4.5H4.5Z" />
+                        <path d="M22,16.5H10a.5.5,0,0,1,0-1H22a.5.5,0,0,1,0,1Z" />
+                        <path d="M16,22.5a.5.5,0,0,1-.5-.5V10a.5.5,0,0,1,1,0V22A.5.5,0,0,1,16,22.5Z" />
+                      </g>
+                    </svg>
+                  </div>
                 </div>
-            </section>
-        </>
-    )
+              </SwiperSlide>
+
+              <SwiperSlide>
+                <div className={styles.card}>
+                  <img src="/images/Personalize/res1.png" alt="" />
+                  <div className={styles.callToAction}>
+                  <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="fill-white"
+                    >
+                      <title />
+                      <g data-name="Layer 2" id="Layer_2">
+                        <path d="M28,28.5H4a.5.5,0,0,1-.5-.5V4A.5.5,0,0,1,4,3.5H28a.5.5,0,0,1,.5.5V28A.5.5,0,0,1,28,28.5Zm-23.5-1h23V4.5H4.5Z" />
+                        <path d="M22,16.5H10a.5.5,0,0,1,0-1H22a.5.5,0,0,1,0,1Z" />
+                        <path d="M16,22.5a.5.5,0,0,1-.5-.5V10a.5.5,0,0,1,1,0V22A.5.5,0,0,1,16,22.5Z" />
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className={styles.card}>
+                  <img src="/images/Personalize/res1.png" alt="" />
+                  <div className={styles.callToAction}>
+                  <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="fill-white"
+                    >
+                      <title />
+                      <g data-name="Layer 2" id="Layer_2">
+                        <path d="M28,28.5H4a.5.5,0,0,1-.5-.5V4A.5.5,0,0,1,4,3.5H28a.5.5,0,0,1,.5.5V28A.5.5,0,0,1,28,28.5Zm-23.5-1h23V4.5H4.5Z" />
+                        <path d="M22,16.5H10a.5.5,0,0,1,0-1H22a.5.5,0,0,1,0,1Z" />
+                        <path d="M16,22.5a.5.5,0,0,1-.5-.5V10a.5.5,0,0,1,1,0V22A.5.5,0,0,1,16,22.5Z" />
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className={styles.card}>
+                  <img src="/images/Personalize/res1.png" alt="" />
+                  <div className={styles.callToAction}>
+                  <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="fill-white"
+                    >
+                      <title />
+                      <g data-name="Layer 2" id="Layer_2">
+                        <path d="M28,28.5H4a.5.5,0,0,1-.5-.5V4A.5.5,0,0,1,4,3.5H28a.5.5,0,0,1,.5.5V28A.5.5,0,0,1,28,28.5Zm-23.5-1h23V4.5H4.5Z" />
+                        <path d="M22,16.5H10a.5.5,0,0,1,0-1H22a.5.5,0,0,1,0,1Z" />
+                        <path d="M16,22.5a.5.5,0,0,1-.5-.5V10a.5.5,0,0,1,1,0V22A.5.5,0,0,1,16,22.5Z" />
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className={styles.card}>
+                  <img src="/images/Personalize/res1.png" alt="" />
+                  <div className={styles.callToAction}>
+                  <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="fill-white"
+                    >
+                      <title />
+                      <g data-name="Layer 2" id="Layer_2">
+                        <path d="M28,28.5H4a.5.5,0,0,1-.5-.5V4A.5.5,0,0,1,4,3.5H28a.5.5,0,0,1,.5.5V28A.5.5,0,0,1,28,28.5Zm-23.5-1h23V4.5H4.5Z" />
+                        <path d="M22,16.5H10a.5.5,0,0,1,0-1H22a.5.5,0,0,1,0,1Z" />
+                        <path d="M16,22.5a.5.5,0,0,1-.5-.5V10a.5.5,0,0,1,1,0V22A.5.5,0,0,1,16,22.5Z" />
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className={styles.card}>
+                  <img src="/images/Personalize/res1.png" alt="" />
+                  <div className={styles.callToAction}>
+                  <svg
+                      viewBox="0 0 32 32"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="fill-white"
+                    >
+                      <title />
+                      <g data-name="Layer 2" id="Layer_2">
+                        <path d="M28,28.5H4a.5.5,0,0,1-.5-.5V4A.5.5,0,0,1,4,3.5H28a.5.5,0,0,1,.5.5V28A.5.5,0,0,1,28,28.5Zm-23.5-1h23V4.5H4.5Z" />
+                        <path d="M22,16.5H10a.5.5,0,0,1,0-1H22a.5.5,0,0,1,0,1Z" />
+                        <path d="M16,22.5a.5.5,0,0,1-.5-.5V10a.5.5,0,0,1,1,0V22A.5.5,0,0,1,16,22.5Z" />
+                      </g>
+                    </svg>
+                  </div>
+                </div>
+              </SwiperSlide>
+            </SwiperJs>
+          </div>
+          <div className={styles.content}>
+            <h2>
+              <b>Nuestro</b> <br /> <span>trabajo</span>
+            </h2>
+            <p>
+              Somos una empresa dedicada al diseño y construcción de espacios,
+              encaminados a lograr un lugar cómodo de acuerdo a tus necesidades.
+              Nuestro principal objetivo es recrear cada diseños de manera
+              autónoma, teniendo en cuenta la inversión y así, poder ejecutar
+              los resultados de cada proyecto de la mejor manera.
+            </p>
+            <a href="#">Contáctanos</a>
+            <div className={styles.mundo}>
+              <img src="/images/apoyo_grafico/mundo.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
