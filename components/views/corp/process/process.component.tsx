1bc32ea22a2adbca97e0aeec2155407097095cac
andres2082113 <andres@kru360.com>
2023-05-15 15:46:52 -0500

    feat: changes added

--- components/views/corp/process/process.component.tsx
+++ components/views/corp/process/process.component.tsx
@@ -3,7 +3,7 @@ import styles from "./process.module.css";
 function Process() {
   return (
     <div className="flex-col sm:flex-col pt-36 md:mb-[-300px] md:flex-row lg:flex-row xl:flex items-center content-center">
-      <div className={"pl-4 lg:pl-32 md:w-8/12"}>
+      <div className={"pl-10 lg:pl-32 md:w-8/12"}>
         <h2 className={"text-5xl md:text-8xl font-black text-white " + styles.title_main}>
           NUESTRO
         </h2>
@@ -11,10 +11,8 @@ function Process() {
           Proceso
         </h3>
         <p className="text-white my-14 w-8/12 md:w-7/12 pl-2 ">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maxime
-          suscipit quod et? Sit harum vitae modi perferendis, minus deleniti
-          adipisci autem, ipsa deserunt quisquam vel praesentium repellendus,
-          veritatis cumque minima.
+          Si quieres remodelar tus espacios, personalizarlos a tu gusto y cuidar cada detalle del diseño,
+          te contamos cómo es el proceso para adquirir nuestros servicios.
         </p>
       </div>
     </div>
--- components/views/home/header/header.component.tsx
+++ components/views/home/header/header.component.tsx
@@ -5,11 +5,12 @@ const Header = () => {
   const route = useRouter();
   return (
     <div className={`w-full flex-col mt-[-110px] ${route.asPath === "/corp" ? "bg-[url('/images/corp/background.png')]" :  "bg-[url('/images/home/background.png')]"} ` + styles.container}>
-      <div className={"flex flex-col content-start items-start pb-10 pt-[140px] " + styles.container_text}>
+      <div className={"flex flex-col content-start items-start pb-10 pt-[100px] md:pt-[140px] " + styles.container_text}>
         <h1 className={"text-5xl ml-2 lg:ml-24 xl:ml-44 md:text-9xl font-black text-white " + styles.title_main}>{route.asPath === "/corp" ? "TU OFICINA" : "REMODELA"}</h1>
         <h3 className={"text-5xl ml-2 lg:ml-24 xl:ml-44 md:text-9xl pl-2 font-medium text-white " + styles.title_main}>{route.asPath === "/corp" ? "corporativa" : "tu cocina"}</h3>
         <p className="text-white ml-2 lg:ml-24 xl:ml-48 md:w-4/12 my-6 w-10/12 pl-2">
-        ¿Te imaginas preparando tus platillos favoritos en una cocina diseñada a tu medida? Una en la que cada detalle será personalizado y perfecto para ti.
+
+          {route.asPath === "/corp" ? "Diseñamos tus espacios según el entorno que lo necesites, cuidando cada detalle para asegurarnos que cumpla con tus expectativas." : "¿Te imaginas preparando tus platillos favoritos en una cocina diseñada a tu medida? Una en la que cada detalle será personalizado y perfecto para ti."}
         </p>
         <button className="ml-4 text-white md:ml-48 px-11 py-2">Contáctanos</button>
       </div>
